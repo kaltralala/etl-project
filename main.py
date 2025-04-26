78f0d2832be59f9d358eb5b364059a6677af9183
Kaltralala <themrhaykal@gmail.com>
2025-04-26 17:19:02 +0700

    Refactor main function to improve ETL process logging and structure

--- main.py
+++ main.py
@@ -1,26 +1,33 @@
 from utils.extract import ProductExtractor, ProductExtractorException
+from utils.transform import DataTransformer
 import logging
+import pandas as pd
 
 # Konfigurasi logging
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 def main():
-    """Fungsi utama untuk menjalankan proses ekstraksi data"""
+    """Fungsi utama untuk menjalankan proses ETL"""
     try:
-        # URL website target
+        # EXTRACT
+        logger.info("Memulai proses ekstraksi data...")
         base_url = "https://fashion-studio.dicoding.dev"
-        
-        # Inisialisasi ekstractor
         extractor = ProductExtractor(base_url)
         
-        # Proses ekstraksi data
-        df = extractor.extract_all_pages()
+        df_raw = extractor.extract_all_pages()
+        raw_path = "raw_fashion_data.csv"
+        df_raw.to_csv(raw_path, index=False)
+        logger.info(f"Berhasil mengekstrak {len(df_raw)} produk ke {raw_path}")
         
-        # Simpan hasil ke CSV
-        output_path = "raw_fashion_data.csv"
-        df.to_csv(output_path, index=False)
-        logger.info(f"Berhasil mengekstrak {len(df)} produk ke {output_path}")
+        # TRANSFORM
+        logger.info("Memulai proses transformasi data...")
+        transformer = DataTransformer()
+        df_clean = transformer.transform_data(df_raw)
+        
+        clean_path = "clean_fashion_data.csv"
+        df_clean.to_csv(clean_path, index=False)
+        logger.info(f"Berhasil menyimpan {len(df_clean)} data bersih ke {clean_path}")
         
     except ProductExtractorException as e:
         logger.error(f"Gagal melakukan ekstraksi data: {str(e)}")
