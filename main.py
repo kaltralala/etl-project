6e6efd8230319c1c539103faa82c7a1b2f32260b
Kaltralala <themrhaykal@gmail.com>
2025-04-28 14:03:41 +0700

    Refactor main function to include data loading process and improve error logging

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-from utils.extract import ProductExtractor, ProductExtractorException
+from utils.extract import ProductExtractor
 from utils.transform import DataTransformer
+from utils.load import DataLoader
 import logging
+import os
 import pandas as pd
 
 # Konfigurasi logging
@@ -29,10 +31,46 @@ def main():
         df_clean.to_csv(clean_path, index=False)
         logger.info(f"Berhasil menyimpan {len(df_clean)} data bersih ke {clean_path}")
         
-    except ProductExtractorException as e:
-        logger.error(f"Gagal melakukan ekstraksi data: {str(e)}")
+        # LOAD
+        logger.info("Memulai proses penyimpanan data...")
+        
+        # Konfigurasi PostgreSQL
+        pg_config = {
+            'host': 'localhost',
+            'database': 'fashion_db',
+            'user': 'developer',
+            'password': 'supersecretpassword',
+            'table_name': 'fashion_products',
+            'port': '5432'
+        }
+        
+        # Masukkan ID spreadsheet
+        SPREADSHEET_ID = "17OXUOijS_TmLZkpL6ogCFVzTk_OBznHdtrwPbcCIzdM"
+
+        # Inisialisasi DataLoader dengan credentials yang sudah ada
+        credentials_path = os.path.join(os.path.dirname(__file__), 'google-sheets-api.json')
+        loader = DataLoader(credentials_path=credentials_path)
+        
+        # Proses loading data
+        results = loader.load_data(
+            df=df_clean,
+            csv_path=clean_path,
+            spreadsheet_id=SPREADSHEET_ID,  # Gunakan spreadsheet ID yang sudah ada
+            pg_config=pg_config
+        )
+        
+        # Log hasil penyimpanan
+        if results['csv_status']:
+            logger.info(f"Data berhasil disimpan ke CSV: {clean_path}")
+        
+        if results['sheets_url']:
+            logger.info(f"Data berhasil disimpan ke Google Sheets: {results['sheets_url']}")
+        
+        if results['postgresql_status']:
+            logger.info("Data berhasil disimpan ke PostgreSQL")
+
     except Exception as e:
-        logger.error(f"Terjadi kesalahan tidak terduga: {str(e)}")
+        logger.error(f"Terjadi kesalahan: {str(e)}")
 
 if __name__ == "__main__":
     main()
